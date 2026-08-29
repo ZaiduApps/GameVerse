e509819d55c1c76a5aebed4eccfa4a048159be6c
ZAIDU <zaidu9527@gmail.com>
2026-08-30 01:06:54 +0800

    fix(gameverse): import topic not found handler

--- src/app/community/topic/[idOrSlug]/page.tsx
+++ src/app/community/topic/[idOrSlug]/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 import CommunityTopicBoardView from './CommunityTopicBoardView';
